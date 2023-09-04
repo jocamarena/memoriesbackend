d4b434a4628c4b839846f12fd58b2604d010d59d
Jose Camarena <jose.j.camarena@gmail.com>
2023-09-03 21:53:09 -0700

    updated UserRestController.java

--- src/main/java/com/example/memoriesbackend/api/UserRestController.java
+++ src/main/java/com/example/memoriesbackend/api/UserRestController.java
@@ -4,23 +4,33 @@ import com.example.memoriesbackend.dto.UserDto;
 import com.example.memoriesbackend.dto.UserRequestDto;
 import com.example.memoriesbackend.model.User;
 import com.example.memoriesbackend.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserRestController {
     private UserService userService;
+    private Logger logger = LoggerFactory.getLogger(UserRestController.class);
+
     public UserRestController(UserService userService) {
         this.userService = userService;
     }
+
     @PostMapping
     public ResponseEntity<UserDto> saveUser(@RequestBody UserRequestDto userRequestDto) {
         userService.getUserByEmail(userRequestDto.getEmail()).ifPresent(user -> {
             throw new RuntimeException("User already exists");
         });
         User user = userService.saveUser(User.builder()
-                .name(userRequestDto.getFirstName() + " " + userRequestDto.getMiddleName() + " " + userRequestDto.getLastName())
+                .firstName(userRequestDto.getFirstName())
+                .middleName(userRequestDto.getMiddleName())
+                .lastName(userRequestDto.getLastName())
                 .email(userRequestDto.getEmail())
                 .role(userRequestDto.getRole())
                 .build());
@@ -32,8 +42,30 @@ public class UserRestController {
                 .active(user.isActive())
                 .build());
     }
-    @GetMapping("/id")
-    public ResponseEntity<UserDto> getUserById(@RequestParam String id) {
+
+    @GetMapping
+    public List<UserDto> getAllUsers() {
+        List<UserDto> users = new ArrayList<>();
+        userService.getAllUsers().stream().forEach(user -> {
+            users.add(UserDto.builder()
+                    .id(user.getId())
+                    .name(user.getFirstName() + " " + user.getMiddleName() + " " + user.getLastName())
+                    .email(user.getEmail())
+                    .role(user.getRole())
+                    .active(user.isActive())
+                    .build());
+        });
+        return users;
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUserById(@PathVariable String id) {
+        userService.delete(Long.parseLong(id));
+        return ResponseEntity.ok("User deleted successfully");
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable String id) {
+        logger.info("getUserById: " + id);
         User user = userService.getUserById(Long.parseLong(id));
         return ResponseEntity.ok(UserDto.builder()
                 .id(user.getId())
@@ -43,8 +75,10 @@ public class UserRestController {
                 .active(user.isActive())
                 .build());
     }
-    @GetMapping("/email")
-    public ResponseEntity<UserDto> getUserByEmail(@RequestParam String email) {
+
+    @GetMapping("/email/{email}")
+    public ResponseEntity<UserDto> getUserByEmail(@PathVariable String email) {
+        logger.info("getUserByEmail: " + email);
         User user = userService.getUserByEmail(email).orElseThrow(() -> new RuntimeException("User not found"));
         return ResponseEntity.ok(UserDto.builder()
                 .id(user.getId())
@@ -54,6 +88,7 @@ public class UserRestController {
                 .active(user.isActive())
                 .build());
     }
+
     @GetMapping("/count")
     public ResponseEntity<Integer> countAllUsers() {
         return ResponseEntity.ok(userService.countAllUsers());
